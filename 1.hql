cdcc5cf5289c871cbfe66464320d4fd65a05fc3c
zhangshengshan <zsszsszsszss@126.com>
2023-12-02 02:18:22 +0800

    fix code !

--- 1.hql
+++
@@ -1,9 +0,0 @@
-select * from
-         (
-         select * from dbo
-         ) AS tmp1
-left join
-         (
-         select * from da.dbp
-         ) AS tmp2
-on tmp1.id = tmp2.id
---
+++ src/main/scala/config/os/OsConfig.scala
@@ -0,0 +1,8 @@
+package config.os
+
+object OsConfig {
+  val macDotPath = "/usr/local/bin/dot"
+  val winDotPath = "C:\\Program Files (x86)\\Graphviz\\bin\\dot.exe"
+  val macOutputPath = "/Users/zhangshengshan/Desktop"
+  val winOutputPath = "C:\\Users\\zhangshengshan\\Desktop"
+}
