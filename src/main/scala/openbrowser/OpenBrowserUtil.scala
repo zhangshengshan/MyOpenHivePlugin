6ab1f12c2399ef936e297f87dc9fa7a3a60423d1
zhangshengshan <zsszsszsszss@126.com>
2023-12-30 23:09:16 +0800

    优化publishing避免重复发布

--- src/main/scala/openbrowser/OpenBrowserUtil.scala
+++ src/main/scala/openbrowser/OpenBrowserUtil.scala
@@ -1,14 +1,27 @@
 package openbrowser
 
 import java.awt.Desktop
-import java.net.URI
+import java.net.{URI, URISyntaxException}
 
 class OpenBrowserUtil
 
 object OpenBrowserUtil {
-  def open(url: String) = {
-    if (Desktop.isDesktopSupported) {
-      Desktop.getDesktop.browse(new URI(url))
+  /**
+   * 打开网页
+   * 如果系统不支持使用Desktop应用，则不执行任何操作。
+   * 如果传入的URL是非法的，会抛出异常。
+   *
+   * @param url 要打开的网页链接
+   */
+  def open(url: String): Unit = {
+    if (Desktop.isDesktopSupported && url != null && !url.trim.isEmpty) {
+      try {
+        Desktop.getDesktop.browse(new URI(url))
+      } catch {
+        case e: URISyntaxException =>
+          println(s"Invalid URL: $url")
+          // 处理URISyntaxException，例如记录日志或通知用户
+      }
     }
   }
 }
