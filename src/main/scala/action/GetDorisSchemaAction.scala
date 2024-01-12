abdef3cf5423796087ebb7ce417a0d8e46592729
MachineLearning <AlphaBetaGo@163.com>
2024-01-12 09:39:01 +0800

    Update README.mdlll
    Signed-off-by: MachineLearning <AlphaBetaGo@163.com>

--- src/main/scala/action/GetDorisSchemaAction.scala
+++ src/main/scala/action/GetDorisSchemaAction.scala
@@ -4,7 +4,9 @@ import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent}
 import hierachyconfig.MyConfigurable
 
 case class Author( login: String, subscriptions_url: String, organizations_url: String)
-
+case class Property(name: String, aggregation_type: String, comment: String, `type`: String)
+case class Data(keysType: String, properties: List[Property], status: Int)
+case class Response(msg: String, code: Int, data: Data, count: Int)
 
 class GetDorisSchemaAction extends AnAction {
   override def actionPerformed(anActionEvent: AnActionEvent): Unit = {
@@ -18,40 +20,44 @@ class GetDorisSchemaAction extends AnAction {
     val port = value.getPort
     val user = value.getUser
     val password = value.getPassword
-
+    //    anneng_ods.ods_bduan_eam_power_station_base_dt_bduan_dashboard
     val yourdb = "yourdb"
-    val yourtb = "youtb"
+    val yourtb = "yourtable"
 
     import java.util.Base64
-    val encoded =
-      Base64.getEncoder.encodeToString((user + ":" + password).getBytes)
+    val encoded = Base64.getEncoder.encodeToString((user + ":" + password).getBytes)
     val headers = Map("Authorization" -> s"Basic $encoded")
 
     val url = s"http://$host:$port/api/$yourdb/$yourtb/_schema"
-//      val response = requests.get(url, headers = headers)
-    val jsongString = os.read(
-      os.Path(
-        "/Users/zhangshengshan/Desktop/MyOpenHivePlugin/ammonite-releases.json"
-      )
-    )
-    val jsonValue = ujson.read(jsongString)
+    val response = requests.get(url, headers = headers)
+    //val jsongString = os.read(
+    //      os.Path(
+    //        "/Users/zhangshengshan/Desktop/MyOpenHivePlugin/ammonite-releases.json"
+    //      )
+    //    )
+    val jsonValue = ujson.read(response.text())
 
     implicit val authorRW = upickle.default.macroRW[Author]
-
-    val author : Author = upickle.default.read[Author](jsonValue(0).obj("author"))
-    println(author)
-    println(author.login)
-
-//    try {
-//      jsonValue.arr
-//        .take(2)
-//        .foreach(item => {
-//          println(item.obj("node_id"))
-//        })
-//    } catch {
-//      case e: Exception => println(e)
-//    }
-//
+    implicit val propertyRW = upickle.default.macroRW[Property]
+    implicit val dataRW = upickle.default.macroRW[Data]
+    implicit val responseRW = upickle.default.macroRW[Response]
+    //    val author : Author = upickle.default.read[Author](jsonValue(0).obj("author"))
+    //    println(author)
+    //    println(author.login)
+    println(jsonValue)
+    val responseObj : Response = upickle.default.read[Response](jsonValue)
+    responseObj.data.properties.foreach(item => {
+      println(item.name)
+    })
+    //    try {
+    //      jsonValue.arr
+    //        .take(2)
+    //        .foreach(item => {
+    //          println(item.obj("node_id"))
+    //        })
+    //    } catch {
+    //      case e: Exception => println(e)
+    //    }
   }
 }
 
