2c21a0585a33b1249c63ad83d4b19864552b867b
MachineLearning <AlphaBetaGo@163.com>
2024-02-07 17:26:42 +0800

    Add Cmd Group Support !

--- src/main/scala/action/MyActionGroup.scala
+++ src/main/scala/action/MyActionGroup.scala
@@ -3,31 +3,34 @@ package action
 import com.intellij.openapi.actionSystem.{ActionGroup, AnAction, AnActionEvent, DefaultActionGroup}
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.ui.messages.MessageDialog
+import misc.ClipBoardUtil
 
 class MyActionGroup extends DefaultActionGroup {
 
   override def isPopup: Boolean = true
   override def getChildren(e: AnActionEvent): Array[AnAction] = {
     Array(
-      new MySubAction1,
-      new MySubAction2
+      new SingleQuoteWrapper,
+      new DoubleQuoteWrapper
     )
   }
 }
 
-class MySubAction1 extends AnAction("子命令1") {
+class SingleQuoteWrapper extends AnAction("单引号") {
   override def actionPerformed(e: AnActionEvent): Unit = {
-
-    Messages.showMessageDialog("子命令1", "Information", Messages.getInformationIcon)
-
-    // 在这里实现子命令1的功能
+    val clipboard = ClipBoardUtil.getFromClipboard
+    val str = clipboard.split("\n").map(x => "'" + x.strip() + "'").mkString("(", ",", ")")
+    Messages.showInfoMessage(str, "剪切板内容")
+    ClipBoardUtil.copyToClipBoard(str)
   }
 }
 
-class MySubAction2 extends AnAction("子命令2"){
+class DoubleQuoteWrapper extends AnAction("双引号"){
   override def actionPerformed(e: AnActionEvent): Unit = {
 
-    Messages.showMessageDialog("子命令2!", "Information", Messages.getInformationIcon)
-    // 在这里实现子命令2的功能
+    val clipboard = ClipBoardUtil.getFromClipboard
+    val str = clipboard.split("\n").map(x => "\"" + x.strip() + "'").mkString(",")
+    Messages.showInfoMessage(str, "剪切板内容")
+    ClipBoardUtil.copyToClipBoard(str)
   }
 }
