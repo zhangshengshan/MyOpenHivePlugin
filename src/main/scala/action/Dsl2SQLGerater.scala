65f9d3568c91cf41a4f7a84759248428da611c0c
MachineLearning <AlphaBetaGo@163.com>
2024-04-01 19:43:58 +0800

    add batch save doris table meta info

--- src/main/scala/action/Dsl2SQLGerater.scala
+++ src/main/scala/action/Dsl2SQLGerater.scala
@@ -12,9 +12,8 @@ class Dsl2SQLGerater extends MyDSLParserBaseVisitor[String]{
   override def visitRoot(ctx: MyDSLParser.RootContext): String = {
     val contexts: util.List[MyDSLParser.Join_relContext] = ctx.join_rel()
     contexts.toList.foreach(x => x.toString())
-
-    ret = ret + ctx.join_rel().toString()
-    return ctx.relation().toString()
+    ret = ret + visit(ctx.relation())
+    ctx.relation().toString()
   }
 
   override def visitJoin_rel(ctx: MyDSLParser.Join_relContext): String = {
@@ -22,6 +21,9 @@ class Dsl2SQLGerater extends MyDSLParserBaseVisitor[String]{
   }
 
   override def visitRelation(ctx: MyDSLParser.RelationContext): String = {
-    null
+
+    // 要根据relation的类型来生成不同的sql
+
+    ctx.toString()
   }
 }
