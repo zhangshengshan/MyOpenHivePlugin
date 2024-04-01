216621570b79369ff9c4921417103ab407bdc949
MachineLearning <AlphaBetaGo@163.com>
2024-04-01 20:26:54 +0800

    add batch save doris table meta info

--- src/main/scala/action/Dsl2SQLGerater.scala
+++ src/main/scala/action/Dsl2SQLGerater.scala
@@ -3,27 +3,60 @@ package action
 import antlr4.dsl.{MyDSLParser, MyDSLParserBaseVisitor}
 
 import java.util
+import scala.collection.JavaConverters.asScalaBufferConverter
 import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 
-class Dsl2SQLGerater extends MyDSLParserBaseVisitor[String]{
+class Dsl2SQLGerater extends MyDSLParserBaseVisitor[String] {
 
   var ret = ""
 
   override def visitRoot(ctx: MyDSLParser.RootContext): String = {
-    val contexts: util.List[MyDSLParser.Join_relContext] = ctx.join_rel()
-    contexts.toList.foreach(x => x.toString())
-    ret = ret + visit(ctx.relation())
-    ctx.relation().toString()
+    ret = ctx.toString()
+    visit(ctx.relation())
+    null
   }
 
   override def visitJoin_rel(ctx: MyDSLParser.Join_relContext): String = {
+    println(ctx.toString())
     ctx.join().toString() + ctx.relation().toString()
   }
 
   override def visitRelation(ctx: MyDSLParser.RelationContext): String = {
+    // TB (join TB)*
+    if (ctx.TB().size() > 0) {
+      val tbList = ctx.TB().asScala.map(_.getText).toList
+      val joinList = ctx.join().asScala.map(visit).toList
+      val combined = tbList
+        .zipAll(joinList, "", "")
+        .flatMap(pair => List(pair._1, pair._2))
+        .mkString(" ")
 
-    // 要根据relation的类型来生成不同的sql
+      ret = ret + combined
+      return combined
+    }
+    // relation join relation
+    else if (ctx.relation().size() > 0) {
+      val relations = ctx.relation().asScala.map(visit).toList
+      val join = ctx.join().asScala.map(visit).toList // change this line
+      val str = relations
+        .zipAll(join, "", "")
+        .flatMap(pair => List(pair._1, pair._2))
+        .mkString(" ")
+      ret = ret + str
+      str
 
-    ctx.toString()
+    }
+    // LP relation RP
+    else if (ctx.LP() != null && ctx.RP() != null) {
+      val relation = visit(ctx.relation(0))
+      ret = ret + s"($relation)"
+      return s"($relation)"
+    } else {
+      return ""
+    }
+  }
+
+  override def visitJoin(ctx: MyDSLParser.JoinContext): String = {
+    return "JOIN"
   }
 }
