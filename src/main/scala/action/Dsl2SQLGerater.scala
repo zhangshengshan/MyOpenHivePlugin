f559b5a3067c24646aad6081c4a712a766daa65e
MachineLearning <AlphaBetaGo@163.com>
2024-04-02 09:26:17 +0800

    add batch save doris table meta info

--- src/main/scala/action/Dsl2SQLGerater.scala
+++ src/main/scala/action/Dsl2SQLGerater.scala
@@ -9,7 +9,6 @@ import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 class Dsl2SQLGerater extends MyDSLParserBaseVisitor[String] {
 
   var ret = ""
-
   override def visitRoot(ctx: MyDSLParser.RootContext): String = {
     val primary_relation: String = visit(ctx.relation())
     ret += primary_relation
@@ -19,7 +18,6 @@ class Dsl2SQLGerater extends MyDSLParserBaseVisitor[String] {
       str
       ret + str
     })
-
     null
   }
 
@@ -33,16 +31,24 @@ class Dsl2SQLGerater extends MyDSLParserBaseVisitor[String] {
   }
 
   override def visitTbJoinTb(ctx: MyDSLParser.TbJoinTbContext): String = {
-
-
-
-    null
+    val tb = ctx.TB(0).getText // 访问第一个 TB
+    val result = new StringBuilder(tb)
+    // 遍历所有的 join TB
+    for (i <- 1 until ctx.TB().size()) {
+      val join = ctx.join(i - 1).getText // 访问 join
+      val tb = ctx.TB(i).getText // 访问 TB
+      result.append(s" $join $tb") // 添加到结果字符串中
+    }
+    result.toString()
   }
 
   override def visitRelationJoinRelation(
       ctx: MyDSLParser.RelationJoinRelationContext
   ): String = {
-    null
+    val relation1 = visit(ctx.relation(0)) // 访问第一个 relation
+    val join = visit(ctx.join()) // 访问 join
+    val relation2 = visit(ctx.relation(1)) // 访问第二个 relation
+    s"$relation1 $join $relation2" // 返回结果字符串
   }
 
   override def visitParenRelation(
