280081b5a4cff2ee2b26af31dd77a8fd3a9da35d
MachineLearning <AlphaBetaGo@163.com>
2024-07-16 11:01:55 +0800

    升级 org.jetbrains.intellij 插件！

--- src/main/scala/action/linage/PlotUtil.scala
+++ src/main/scala/action/linage/PlotUtil.scala
@@ -7,6 +7,7 @@ import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.VirtualFile
 import config.os.OsConfig
+import myui.MyMultiChoiceDialog
 
 object PlotUtil {
   def plotAction(
@@ -31,16 +32,35 @@ object PlotUtil {
     }
 
     // sourceList 作为筛选框选择选项
-    val source = Messages
-      .showEditableChooseDialog(
-        "请选择一个表格作为分析对象",
-        "选择表",
-        Messages.getInformationIcon,
-        sourceList.toArray,
-        sourceList.head,
-        null
-      )
-      .strip()
+//    val source = Messages
+//      .showEditableChooseDialog(
+//        "请选择一个表格作为分析对象",
+//        "选择表",
+//        Messages.getInformationIcon,
+//        sourceList.toArray,
+//        sourceList.head,
+//        null
+//      )
+//      .strip()
+
+    import scala.jdk.CollectionConverters._
+
+    val scalaList: List[String] = sourceList
+    val javaList: java.util.List[String] = scalaList.asJava
+
+    val myMultiChoiceDialog: MyMultiChoiceDialog = new MyMultiChoiceDialog(
+      javaList
+    )
+    myMultiChoiceDialog.show()
+
+    val source = myMultiChoiceDialog.isOK match {
+      case true =>
+        myMultiChoiceDialog.getSelectedOptions.toArray().toList.head.toString
+      case _ => {
+        Messages.showInfoMessage("没有选择任何一个表格作为分析对象， 退出处理程序", "Information")
+        return
+      }
+    }
 
     // source 如果是 db.tb 这种形式取 tb
     val tableName = source.split("\\.") match {
