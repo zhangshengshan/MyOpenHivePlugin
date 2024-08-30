a73a60fee32c5b671a9b16d8bee2ae4a40c8df14
MachineLearning <AlphaBetaGo@163.com>
2024-08-30 09:50:52 +0800

    升级 org.jetbrains.intellij 插件！

--- src/main/scala/action/hilight/HighlightWordService.scala
+++ src/main/scala/action/hilight/HighlightWordService.scala
@@ -106,22 +106,47 @@ final class HighlightWordService {
     // Update the color index for the next highlight
   }
 
+  /**
+   * 在所有打开的文件中清除高亮标记
+   *
+   * 此函数的目的是遍历所有打开的文件和编辑器，清除每个文件中的高亮标记
+   * 它首先获取所有打开的文件，然后获取所有编辑器，通过比较文件和编辑器中的文件实例，
+   * 确保正确的文件对应的编辑器被找到，然后调用私有函数 clearHighlightsInEditor 来清除高亮标记
+   *
+   * @param project 当前项目，用于获取打开的文件和编辑器
+   */
   def clearHighlightsInAllOpenFiles(project: Project): Unit = {
+    // 获取当前项目中所有打开的文件
     val openFiles = OpenFilesUtil.getAllOpenFiles(project)
+    // 遍历所有打开的文件
     for (file <- openFiles) {
+      // 获取所有编辑器实例
       val editors = OpenFilesUtil.getAllEditors
+      // 遍历所有编辑器
       for (editor <- editors) {
+        // 检查当前编辑器中的文件是否与当前遍历的文件相同
         if (
           FileDocumentManager.getInstance().getFile(editor.getDocument) == file
         ) {
+          // 清除当前编辑器中的高亮标记
           clearHighlightsInEditor(editor)
         }
       }
     }
   }
 
+  /**
+   * 在指定的编辑器中清除所有高亮标记
+   *
+   * 此函数通过获取编辑器的标记模型，然后调用 removeAllHighlighters 方法来清除所有高亮标记
+   * 它是一个辅助函数，用于 clearHighlightsInAllOpenFiles 函数中清除每个编辑器中的高亮标记
+   *
+   * @param editor 需要清除高亮标记的编辑器
+   */
   private def clearHighlightsInEditor(editor: Editor): Unit = {
+    // 获取编辑器的标记模型
     val markupModel = editor.getMarkupModel
+    // 清除标记模型中的所有高亮标记
     markupModel.removeAllHighlighters()
   }
 
