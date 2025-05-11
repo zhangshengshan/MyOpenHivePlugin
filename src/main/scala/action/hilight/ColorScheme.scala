5480c1b1316753264641f1b966472a8238c9e05e
zhangshengshan <zsszsszsszss@126.com>
2025-05-11 15:46:04 +0800

    feat(action): 添加数据质量检查功能
    - 新增 DataQualityCheck 类，实现数据质量检查的 SQL生成和 API 调用- 在 plugin.xml 中添加数据质量检查的快捷键和菜单项- 优化代码结构，增加注释和文档说明

--- src/main/scala/action/hilight/ColorScheme.scala
+++ src/main/scala/action/hilight/ColorScheme.scala
@@ -6,8 +6,6 @@ object ColorScheme {
 
   // 定义一个私有的颜色数组，用于存储字体颜色
   val FONT_COLORS: Array[Color] = Array(
-    new Color(255, 255, 255), // White
-    new Color(0, 0, 0), // Black
     new Color(255, 0, 0), // Red
     new Color(0, 255, 0), // Green
     new Color(0, 0, 255), // Blue
@@ -25,8 +23,6 @@ object ColorScheme {
 
   // 定义一个私有的颜色数组，用于存储背景颜色，其中的颜色是根据字体颜色的补色原则选择的
   val BACKGROUD_COLORS: Array[Color] = Array(
-    new Color(0, 0, 0), // Black (complement of White)
-    new Color(255, 255, 255), // White (complement of Black)
     new Color(0, 255, 255), // Cyan (complement of Red)
     new Color(255, 0, 255), // Magenta (complement of Green)
     new Color(255, 255, 0), // Yellow (complement of Blue)
