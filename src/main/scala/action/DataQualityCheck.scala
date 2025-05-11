d2c85529f8f7cda84e2206891e9fdaa0b1e5576f
zhangshengshan <zsszsszsszss@126.com>
2025-05-11 10:03:04 +0800

    feat(action): 添加数据质量检查功能
    - 新增 DataQualityCheck 类，实现数据质量检查的 SQL生成和 API 调用- 在 plugin.xml 中添加数据质量检查的快捷键和菜单项- 优化代码结构，增加注释和文档说明

---
+++ src/main/scala/action/DataQualityCheck.scala
@@ -0,0 +1,166 @@
+package action
+
+import action.util.EditorUtil
+import another.ClusDbTbNode
+import com.intellij.openapi.actionSystem.{
+  AnAction,
+  AnActionEvent,
+  CommonDataKeys
+}
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileChooser.{
+  FileChooserDescriptor,
+  FileChooserDialog,
+  FileChooserFactory
+}
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.{PsiDocumentManager, PsiFile}
+import com.zss.graph.{Graph, Node}
+import config.os.OsConfig
+import hierachyconfig.MyConfigurable
+import misc.ClipBoardUtil
+
+//case class Property(
+//    name: String,
+//    aggregation_type: String,
+//    comment: String,
+//    `type`: String
+//)
+//case class Data(keysType: String, properties: List[Property], status: Int)
+//case class Response(msg: String, code: Int, data: Data, count: Int)
+
+class DataQualityCheck extends AnAction {
+  final val DOT = "\\."
+
+  /** @param responseObj
+    * @param db
+    * @param tb
+    * @return
+    */
+  def GenDataCheckSQL(
+      responseObj: Response,
+      db: String,
+      tb: String
+  ): String = {
+
+    val maxLength =
+      responseObj.data.properties.map(item => item.name.length + 2).max
+    val maxCommentLength = 2 * maxLength + 4
+
+    val SEP = System.lineSeparator()
+
+    def fillAsSpaces(str: String): String = {
+      val spaceNum = maxLength - str.length
+      " " * spaceNum
+    }
+
+    def fillCommentSpaces(str: String): String = {
+      val spaceNum = maxCommentLength - str.length + maxLength
+      " " * spaceNum
+    }
+
+    val sqlStyle = Messages.showYesNoDialog(
+      "Do you want to use Doris SQL style?",
+      "SQL Style",
+      Messages.getQuestionIcon
+    )
+
+    def getAliasName(item: Property): String = {
+      if (item.comment == null || item.comment.isEmpty) {
+        item.name
+      } else {
+        "`" + item.comment + "`"
+      }
+    }
+
+    val selectList = responseObj.data.properties
+      .map { item =>
+        s"""COUNT(DISITNCT ${item.name} ) AS ${item.name}_cnt """
+      }
+      .mkString("\t,")
+
+    s"SELECT${SEP}\t $selectList${SEP}FROM${SEP}\t$db.$tb ;"
+  }
+  override def actionPerformed(anActionEvent: AnActionEvent): Unit = {
+    val value: MyConfigurable = MyConfigurable.getInstance()
+    val host = value.getHost
+    val port = value.getPort
+    val user = value.getUser
+    val password = value.getPassword
+    val editor: Editor = anActionEvent.getData(CommonDataKeys.EDITOR)
+
+    val selectText = editor.getSelectionModel.getSelectedText
+    val model = editor.getCaretModel
+    val offset = model.getOffset
+    val project: Project = editor.getProject
+    val psiFile: PsiFile =
+      PsiDocumentManager getInstance project getPsiFile editor.getDocument;
+
+    val expandObj = EditorUtil.getCaretUnderCursor(editor)
+
+    var db = ""
+    var tb = ""
+    if (
+      expandObj != null && expandObj
+        .contains(".") && expandObj.split("\\.").length == 2
+    ) {
+      val strings = expandObj.split("\\.")
+      db = strings(0)
+      tb = strings(1)
+    } else if (
+      selectText != null && selectText
+        .contains(".") && selectText.split("\\.").length == 2
+    ) {
+      val strings = selectText.split("\\.")
+      db = strings(0)
+      tb = strings(1)
+    } else {
+      // intellij pop up a input to get the db and table name
+      val str = Messages.showInputDialog(
+        "Please input the db and table name",
+        "Input Dialog",
+        Messages.getQuestionIcon
+      )
+      val strings = str.split("\\.")
+      db = strings(0)
+      tb = strings(1)
+    }
+    val yourdb = db
+    val yourtb = tb
+    println(yourdb)
+    println(yourtb)
+
+    import java.util.Base64
+    val encoded =
+      Base64.getEncoder.encodeToString((user + ":" + password).getBytes)
+    val headers = Map("Authorization" -> s"Basic $encoded")
+
+    val url = s"http://$host:$port/api/$yourdb/$yourtb/_schema"
+    val response = requests.get(url, headers = headers)
+    val jsonValue = ujson.read(response.text())
+
+    if (jsonValue("code").num != 0) {
+      Messages.showInfoMessage(jsonValue("msg").str, "Error")
+      return
+    }
+
+    implicit val propertyRW = upickle.default.macroRW[Property]
+    implicit val dataRW = upickle.default.macroRW[Data]
+    implicit val responseRW = upickle.default.macroRW[Response]
+    println(jsonValue)
+    val responseObj: Response = upickle.default.read[Response](jsonValue)
+    responseObj.data.properties.foreach(item => {
+      println(item.name)
+    })
+
+    val sql = GenDataCheckSQL(responseObj, yourdb, yourtb)
+
+    Messages.showInfoMessage(sql, "Generated Doris SQL")
+    ClipBoardUtil.copyToClipBoard(sql)
+  }
+}
+
+// define a case class as above json
